35453544550f0927a07b20fe4c6f84bb9240326f
Lika Snopok <Lika.Fleur@yandex.by>
2024-06-06 11:40:23 +0300

    !

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,6 +17,7 @@ class Footer extends React.Component {
                 <li><a href="https://vk.com/">VK</a></li>
                 <li><a href="https://www.youtube.com/">YouTube</a></li>
             </ul>
+            <a href="https://mrqz.me/665454413078b0026ef08b0">Помогите нам стать лучше</a>
         </footer>)
     };
 }
